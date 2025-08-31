73942011d40501b98bf2a28d5888940a5a402b7c
itskvsin <kevinsolankistudy@gmail.com>
2025-08-31 17:32:41 +0530

    Add GSAP animations and dependencies; remove unused asset

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
+  useGSAP(() => {
+    gsap.to("#text", {
+      ease: 'power1.inOut',
+      opacity: 1,
+      y: 0
+    })
+
+    gsap.fromTo(".para", {
+      opacity: 0,
+      y: 20
+    }, {
+      opacity: 1,
+      y: 0,
+      ease: 'power1.inOut',
+      stagger: 0.3,
+      delay: 0.5
+    })
+  }, [])
 
   return (
     <main>
